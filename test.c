235b2094ecbbb60bfa96282c044c6c61045310d6
hello tui <33209790+TUISYS@users.noreply.github.com>
2022-04-13 10:13:49 +0800

    JGP,PNG图片解码测试
    这样设置避免界面刷新重复解码

--- test.c
+++ test.c
@@ -1,6 +1,32 @@
 
 #include "tui.h"
 #include "jpeg_decode_interface.h"
+#include "png_decode_interface.h"
+
+void tui_png_decode_test(void)
+{
+	tui_image_attri_t attri = { 0 };
+	tui_obj_t * image;
+	unsigned int img_w, img_h;
+	unsigned char *png_rgba_buf;
+	
+	/* 解析JPG */
+	png_rgba_buf = tui_png_decode_pngfile_to_argbbuf("F:\\cool.png", &img_w, &img_h);
+	if (jpeg_rgb_buf) {
+		/* 创建image对象 */
+		image = tui_image_create(tui_layer_top());
+		/* 设置属性 */
+		memset(&attri, 0, sizeof(tui_image_attri_t));
+		attri.obj.pt.x = 50;
+		attri.obj.pt.y = 50;
+		tui_image_set_attri(image, &attri);
+		tui_image_set_image_rgba_buf(image, 0, png_rgba_buf, img_w, img_h);
+		tui_image_set_cur_img_index(image, 0);
+
+		/* 释放解析JPG的buffer */
+		tui_png_decode_free_rgbabuf(png_rgba_buf);
+	}
+}
 
 void tui_jpeg_decode_test(void)
 {
@@ -12,7 +38,7 @@ void tui_jpeg_decode_test(void)
 	int flen;
 	
 	/* 解析JPG */
-	jpeg_rgb_buf = tui_jpeg_decode_jpegfile_to_argbbuf("F:\\Penguins.jpg", &img_w, &img_h, TUI_JPEG_SCALE_1_4);
+	jpeg_rgb_buf = tui_jpeg_decode_jpegfile_to_argbbuf("F:\\qq.jpg", &img_w, &img_h, TUI_JPEG_SCALE_1_4);
 	if (jpeg_rgb_buf) {
 		/* 创建image对象 */
 		image = tui_image_create(tui_layer_top());
@@ -20,9 +46,6 @@ void tui_jpeg_decode_test(void)
 		memset(&attri, 0, sizeof(tui_image_attri_t));
 		attri.obj.pt.x = 50;
 		attri.obj.pt.y = 50;
-		attri.obj.size.width = img_w;
-		attri.obj.size.height = img_h;
-		attri.img_num = 1;
 		tui_image_set_attri(image, &attri);
 		tui_image_set_image_argb_buf(image, 0, jpeg_rgb_buf, img_w, img_h);
 		tui_image_set_cur_img_index(image, 0);
@@ -31,7 +54,7 @@ void tui_jpeg_decode_test(void)
 		tui_jpeg_decode_free_argbbuf(jpeg_rgb_buf);
 	}
 
-	fjpg = fopen("F:\\Koala.jpg", "rb");
+	fjpg = fopen("F:\\qq.jpg", "rb");
 	if (fjpg) {
 		fseek(fjpg, 0, SEEK_END);
 		flen = ftell(fjpg);
@@ -54,9 +77,6 @@ void tui_jpeg_decode_test(void)
 				memset(&attri, 0, sizeof(tui_image_attri_t));
 				attri.obj.pt.x = 400;
 				attri.obj.pt.y = 50;
-				attri.obj.size.width = img_w;
-				attri.obj.size.height = img_h;
-				attri.img_num = 1;
 				tui_image_set_attri(image, &attri);
 				tui_image_set_image_argb_buf(image, 0, jpeg_rgb_buf, img_w, img_h);
 				tui_image_set_cur_img_index(image, 0);
